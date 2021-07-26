0623c8b87ae54ad15b166279cdfce5bb0db7e9d2
AFSIN ASIF <afsnasif@gmail.com>
2021-07-26 13:14:43 +0530

    Login and dashboard routing , created register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,53 +7,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  aim ="Your banking partner"
-  accnum="account number"
-  acno=""
-  password=""
+  aim = "Your banking partner"
+  accnum = "account number"
+  acno = "Enter acc/no here"
+  password = ""
 
-  user:any ={
-    1000:{acno: 1000, username:"Akhil", password :"userone", balance:2000},
-    1001:{acno: 1000, username:"Anhil", password :"usertwo", balance:3000},
-    1002:{acno: 1000, username:"Akhila", password :"userthree", balance:1000},
-    1003:{acno: 1000, username:"Neena", password :"userfour", balance:4000},
-    1004:{acno: 1000, username:"Susha", password :"userfive", balance:5000}
+  user: any = {
+    1000: { acno: 1000, username: "Akhil", password: "userone", balance: 2000 },
+    1001: { acno: 1000, username: "Anhil", password: "usertwo", balance: 3000 },
+    1002: { acno: 1000, username: "Akhila", password: "userthree", balance: 1000 },
+    1003: { acno: 1000, username: "Neena", password: "userfour", balance: 4000 },
+    1004: { acno: 1000, username: "Susha", password: "userfive", balance: 5000 }
   }
-  constructor() { }
+  constructor( private router:Router) {}
+  
 
   ngOnInit(): void {
   }
-  login(a:any, p:any){
+  // login(a:any, p:any){
+  //   // alert("Login Clicked")
+  //   var acno= a.value;
+  //   console.log(a.value);
+
+  //   var pswd = p.value;
+  //   console.log(p.value);
+
+  //   let accDetails = this.user;
+  //   if(acno in accDetails){
+  //     if(pswd == accDetails[acno]["password"])
+  //     {
+  //       alert("Login Successful")
+  //     }
+  //     else{
+  //       alert("Incorrect Password")
+  //     }
+  //   }
+  //   else{
+  //     alert("Incorrect Username/accno")
+  //   }
+
+
+  login() {
     // alert("Login Clicked")
-    var acno= a.value;
-    console.log(a.value);
-    
-    var pswd = p.value;
-    console.log(p.value);
-    
+    var acno = this.acno;
+
+    var pswd = this.password;
+
     let accDetails = this.user;
-    if(acno in accDetails){
-      if(pswd == accDetails[acno]["password"])
-      {
+    if (acno in accDetails) {
+      if (pswd == accDetails[acno]["password"]) {
         alert("Login Successful")
+        this.router.navigateByUrl("dashboard")
       }
-      else{
+      else {
         alert("Incorrect Password")
       }
     }
-    else{
+    else {
       alert("Incorrect Username/accno")
     }
-
-    
-
-
-
   }
+  
   // accChange(event:any){
   //   console.log(event.target.value);
   //   this.acno= event.target.value;
-    
+
   // }
   // pswdChange(event:any){
   //   console.log(event.target.value);
