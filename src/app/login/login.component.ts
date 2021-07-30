6ce5ee8c3f5d02b7a85e800b54ef0c0c66eefbbc
AFSIN ASIF <afsnasif@gmail.com>
2021-07-30 23:16:23 +0530

    before data service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,10 +14,10 @@ export class LoginComponent implements OnInit {
 
   user: any = {
     1000: { acno: 1000, username: "Akhil", password: "userone", balance: 2000 },
-    1001: { acno: 1000, username: "Anhil", password: "usertwo", balance: 3000 },
-    1002: { acno: 1000, username: "Akhila", password: "userthree", balance: 1000 },
-    1003: { acno: 1000, username: "Neena", password: "userfour", balance: 4000 },
-    1004: { acno: 1000, username: "Susha", password: "userfive", balance: 5000 }
+    1001: { acno: 1001, username: "Anhil", password: "usertwo", balance: 3000 },
+    1002: { acno: 1002, username: "Akhila", password: "userthree", balance: 1000 },
+    1003: { acno: 1003, username: "Neena", password: "userfour", balance: 4000 },
+    1004: { acno: 1004, username: "Susha", password: "userfive", balance: 5000 }
   }
   constructor( private router:Router) {}
   
